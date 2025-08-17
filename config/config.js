e99512607ae8ad43a0e4ba4e8619981a6c3845f1
Ayush Rai <145470522+ayush123-bit@users.noreply.github.com>
2025-08-17 15:34:44 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 module.exports = {
   geminiApiKey: process.env.GEMINI_API_KEY,
   emailUser: process.env.EMAIL_USER,
